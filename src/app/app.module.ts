5120c50cd46782c1d43814a737b03149000b5c15
i400858 <400858@student.fontys.nl>
2020-04-26 14:06:00 +0200

    first version website angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {PortfolioComponent }from './portfolio/portfolio.component';
+import {MatStepperModule} from '@angular/material/stepper';
+
+
+const appRoutes: Routes =[
+  {path:'about', component:AboutComponent},
+  {path: 'portfolio',component:PortfolioComponent},
+  {path:'',redirectTo:'/about',pathMatch:'full'}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    AboutComponent,
+    PortfolioComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatStepperModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
